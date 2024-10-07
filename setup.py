dd1e73fe799280aea4276ec24a302581c0b98de5
vladiolus3 <75817876+vladiolus3@users.noreply.github.com>
2024-10-08 01:26:40 +0300

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py-jup-decoder',
-    version='1.0.0',
+    version='0.1.0',
     description='Copied from https://github.com/jup-ag/instruction-parser',
     url='https://github.com/vladiolus3/jupiterDecoder',
     packages=['src'],
